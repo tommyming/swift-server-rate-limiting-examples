d586c38b7295015fdf1213aa5f42a49ebadb9bc1
Tommy Han <tommyhonym@gmail.com>
2024-07-19 12:54:38 +0800

    Update readme and gitignore, added TokenBucket

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # swift-server-rate-limiting-examples
 a simple repository that showing the possibility to deploy rate limiting algorithms using swift, for servers.
+
+
+Reference: https://blog.algomaster.io/p/rate-limiting-algorithms-explained-with-code
---
+++ TokenBucket.swift
@@ -0,0 +1,29 @@
+// Token Bucket
+
+import Foundation
+
+// Bucket Structure
+struct TokenBucket: Equatable {
+    let capacity: Int
+    let fillRate: Int
+    var tokens: Int
+    var lastRefillTimeStamp: Date = Date.now
+
+    func allowRequest(incoming: Int) -> Bool {
+        refill()
+
+        guard tokens > incoming else { return false }
+
+        tokens -= incoming
+        return true
+    }
+
+    private mutating func refill() {
+        let currentTime = Date.now
+        let tokensToAdd = (currentTime.timeIntervalSince1970 - lastRefillTimeStamp.timeIntervalSince1970) * fillRate / 1000
+        tokens = min(capacity, tokens + tokensToAdd)
+        lastRefillTimeStamp = currentTime
+    }
+}
+
+// MARK: You might need a timer to keep the fill rate as constant.
